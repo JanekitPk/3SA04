493367e19c31a43f245526ad18dd0743579f5c99
JanekitPk <6210110051@psu.ac.th>
2021-07-12 10:26:04 +0700

    Push 4

--- components/Weather.js
+++ components/Weather.js
@@ -2,7 +2,26 @@ import React from 'react'
 import {Text} from 'react-native'
 
 export default function Weather(props) {
+    const [forecastInfo,setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+    })
+
     return (
-    <Text>{props.zipCode}</Text>
+        //แปลงข้อมูลทั้งหมดเป็น props ให้ Forecast.js
+        <ImageBackground source={require('../bg.png')} style={styles.backdrop}>
+            <Text>Zip Code</Text>
+            <Text>{props.zipCode}</Text>
+            <Forecast {...forecastInfo}/> 
+        </ImageBackground>
     );
    }
+
+   const styles = StyleSheet.create({    //กำหนด backdrop style 
+        backdrop: {
+            alignItems: 'center',
+            width: '100%',
+            height:'100%'
+        }
+   })
