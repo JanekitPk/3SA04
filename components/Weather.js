651ac9102929df07baefc5e98af85ae2f914e38c
JanekitPk <6210110051@psu.ac.th>
2021-07-12 11:27:06 +0700

    Push 7

--- components/Weather.js
+++ components/Weather.js
@@ -1,8 +1,25 @@
 import React,{useState} from 'react'
-import {ImageBackground, Text, StyleSheet} from 'react-native'
+import {ImageBackground, Text, StyleSheet, View} from 'react-native'
 import Forecast from './Forecast';
 
 export default function Weather(props) {
+    /*useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=...`)
+            .then((response) => response.json())
+            .then((json) => {
+                setForecastInfo({
+                    main: json.weather[0].main,
+                    description: json.weather[0].description,
+                    temp: json.main.temp});
+                })
+            .catch((error) => {
+                console.warn(error);
+            });
+        }
+    }, [props.zipCode])
+*/
     const [forecastInfo,setForecastInfo] = useState({
         main: 'main',
         description: 'description',
@@ -12,22 +29,30 @@ export default function Weather(props) {
     return (
         //แปลงข้อมูลทั้งหมดเป็น props ให้ Forecast.js
         <ImageBackground source={require('../bg.png')} style={styles.backdrop}>
-            <Text style={styles.BText}>Zip Code</Text>
-            <Text style={styles.BText}>{props.zipCode}</Text>
-            <Forecast {...forecastInfo}/> 
+            <View style={styles.BBlack}>
+                <Text style={styles.BText}>Zip Code</Text>
+                <Text style={styles.BText}>{props.zipCode}</Text>
+                <Forecast {...forecastInfo}/> 
+            </View>
         </ImageBackground>
     );
    }
 
+
    const styles = StyleSheet.create({    //กำหนด backdrop style 
         backdrop: {
             flexDirection: 'column',
-            justifyContent: 'center', //center แนวแกนตั้ง
-            alignItems: 'center', //center แนวแกนนอน
             width: '100%',
             height:'100%'
         },
         BText: {
             fontSize: 40
+        },
+        BBlack:{
+            justifyContent: 'center', //center แนวแกนตั้ง
+            alignItems: 'center', //center แนวแกนนอน
+            backgroundColor: 'rgba(0,0,0,0.3)',
+            height:'50%',
+            width:'100%'
         }
    })
